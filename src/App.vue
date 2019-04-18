6b440ac7f1cb85cf97b1636d41503c558f336880
Lawrence Davis <lawrence@lrdiv.co>
2019-04-17 20:10:06 -0500

    Fix admin nav link, remove vue-axios

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/admin">Admin</router-link>
     </div>
     <router-view/>
   </div>
@@ -27,3 +27,8 @@
   }
 }
 </style>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend();
+</script>
